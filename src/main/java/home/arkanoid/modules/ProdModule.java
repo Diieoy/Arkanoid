c7b021c9cb1a1f02403ceeb50d71b51b5f1e2e66
Diieoy <mrvipandreika@mail.ru>
2017-03-30 20:53:46 +0300

    add ProdModule

---
+++ src/main/java/home/arkanoid/modules/ProdModule.java
@@ -0,0 +1,48 @@
+package home.arkanoid.modules;
+
+
+import com.google.inject.AbstractModule;
+import com.google.inject.Provides;
+import com.google.inject.Singleton;
+import com.google.inject.TypeLiteral;
+import home.arkanoid.dao.AbstractDAO;
+import home.arkanoid.dao.db.MatchDAO;
+import home.arkanoid.dao.db.PlayerDAO;
+import home.arkanoid.entities.Match;
+import home.arkanoid.entities.Player;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+
+import java.io.IOException;
+import java.util.Properties;
+
+public class ProdModule extends AbstractModule{
+    @Override
+    protected void configure() {
+        bind(new TypeLiteral<AbstractDAO<Player>>(){}).to(PlayerDAO.class);
+        bind(new TypeLiteral<AbstractDAO<Match>>(){}).to(MatchDAO.class);
+    }
+
+    @Provides
+    @Singleton
+    public SessionFactory provideSessionFactory(){
+
+        Properties properties = new Properties();
+
+        try {
+            properties.load(getClass().getClassLoader().getResourceAsStream("hibernate.properties"));
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+
+        Configuration cfg = new Configuration().addProperties(properties);
+        cfg = cfg.addAnnotatedClass(Player.class)
+                .addAnnotatedClass(Match.class);
+
+        ServiceRegistry sr = new StandardServiceRegistryBuilder().applySettings(cfg.getProperties()).build();
+
+        return cfg.buildSessionFactory(sr);
+    }
+}
